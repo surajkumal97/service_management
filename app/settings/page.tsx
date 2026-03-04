981fc79535b69d2b04f780196f0e6e1ec67740e7
Saroj Adhikari <sarojadh1111@gmail.com>
2026-03-04 22:19:21 +0545

    Setting

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,4 +1,47 @@
-const Settings = () => {
-  return <>Settings</>;
+import Container from "../components/container";
+import Items from "../components/items";
+import { IconName } from "lucide-react/dynamic";
+
+const SettingsDashboard = () => {
+  interface Setting {
+    name: string;
+    description: string;
+    icon: IconName;
+    route: string;
+  }
+
+  const settings: Setting[] = [
+    {
+      name: "User Dashboard",
+      description: "Customize your working environment",
+      icon: "layout-dashboard",
+      route: "userdashboard",
+    },
+    {
+      name: "Role Management",
+      description: "Define user permissions and access",
+      icon: "users",
+      route: "rolemanagement",
+    },
+    {
+      name: "User Accounts",
+      description: "Create and manage user profiles",
+      icon: "user-check",
+      route: "useraccounts",
+    },
+    {
+      name: "System Settings",
+      description: "Advanced configuration options",
+      icon: "settings",
+      route: "systemsettings",
+    },
+  ];
+
+  return (
+    <Container>
+      <Items clients={settings} cols={2} />
+    </Container>
+  );
 };
-export default Settings;
+
+export default SettingsDashboard;
